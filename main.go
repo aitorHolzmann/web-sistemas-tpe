81da8534d267acbf4064b9207913feade428d834
Nicolas Benito <nicobenito28@gmail.com>
2026-08-27 10:20:25 -0300

    Correji el error de https:// por http:// del print de servidor escuchando en..

--- main.go
+++ main.go
@@ -5,18 +5,18 @@ import (
 	"net/http"
 )
 
-func main(){
-  dirPath := "./static"
-  server := http.FileServer(http.Dir(dirPath))
+func main() {
+	dirPath := "./static"
+	server := http.FileServer(http.Dir(dirPath))
 
-  http.Handle("/", server)
-  
-  port := ":8080"
-  fmt.Printf("Servidor escuchand en https://localhost%s\n", port)
+	http.Handle("/", server)
 
-  err := http.ListenAndServe(port, nil)
-  if (err != nil){
-    fmt.Printf("Error al iniciar el servidor: %s\n", err)
-  }
+	port := ":8080"
+	fmt.Printf("Servidor escuchand en http://localhost%s\n", port)
+
+	err := http.ListenAndServe(port, nil)
+	if err != nil {
+		fmt.Printf("Error al iniciar el servidor: %s\n", err)
+	}
 
 }
